7f7b7b2369c308d746065d985a4dc65ae2a9b6bd
Halil Onur Çeçen <cecenonur0@gmail.com>
2018-12-23 00:14:29 +0300

    added db as a library and changed interface arguments

--- OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
+++ OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
@@ -3,23 +3,19 @@ package com.server.orchestration;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import com.db.biztalk.*;
+
 import javax.xml.ws.Endpoint;
 
 @WebService(name = "OrchestrationService", targetNamespace = "")
 public interface IOrchestrationService {
 
     @WebMethod(action = "add_orchestration", operationName = "addOrchestration")
-    boolean addOrchestration(@WebParam(name = "from") String from);
+    boolean addOrchestration(@WebParam(name = "value") Orchestration value);
 
     @WebMethod(action = "add_job", operationName = "addJob")
-    boolean addJob(@WebParam(name = "from") String from);
+    boolean addJob(@WebParam(name = "value") Job value);
 
     @WebMethod(action = "add_rule", operationName = "addRule")
-    boolean addRule(@WebParam(name = "from") String from);
-
-    public static void main(String[] argv) {
-        Object implementor = new OrchestrationService();
-        String address = "http://localhost:9000/HelloWorld";
-        Endpoint.publish(address, implementor);
-    }
+    boolean addRule(@WebParam(name = "value") Rule value);
 }
--- OrchestrationService/src/com/server/orchestration/OrchestrationService.java
+++ OrchestrationService/src/com/server/orchestration/OrchestrationService.java
@@ -1,22 +1,27 @@
 package com.server.orchestration;
 
+import com.db.biztalk.Job;
+import com.db.biztalk.Orchestration;
+import com.db.biztalk.Rule;
+
 import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
 
 @WebService(endpointInterface = "com.server.orchestration.IOrchestrationService")
 public class OrchestrationService implements IOrchestrationService {
 
     @Override
-    public boolean addOrchestration(String from){
+    public boolean addOrchestration(Orchestration value){
         return false;
     }
 
     @Override
-    public boolean addJob(String from){
+    public boolean addJob(Job value){
         return false;
     }
 
     @Override
-    public boolean addRule(String from){
+    public boolean addRule(Rule value){
         return false;
     }
 }
