b29bfa5bb77f4b5a6dfa03578d43742ac1a37e11
Halil Onur Çeçen <cecenonur0@gmail.com>
2018-12-23 01:36:25 +0300

    Added required annotation for arguments

--- OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
+++ OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
@@ -5,17 +5,18 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import com.db.biztalk.*;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.ws.Endpoint;
 
 @WebService(name = "OrchestrationService", serviceName = "OrchestrationService", targetNamespace = "http://localhost:9000/HelloWorld", wsdlLocation = "http://localhost:9000/HelloWorld")
 public interface IOrchestrationService {
 
     @WebMethod(action = "add_orchestration", operationName = "addOrchestration")
-    boolean addOrchestration(@WebParam(name = "value") Orchestration value);
+    boolean addOrchestration(@WebParam(name = "value") @XmlElement(required = true) Orchestration value);
 
     @WebMethod(action = "add_job", operationName = "addJob")
-    boolean addJob(@WebParam(name = "value") Job value);
+    boolean addJob(@WebParam(name = "value") @XmlElement(required = true) Job value);
 
     @WebMethod(action = "add_rule", operationName = "addRule")
-    boolean addRule(@WebParam(name = "value") Rule value);
+    boolean addRule(@WebParam(name = "value") @XmlElement(required = true) Rule value);
 }
