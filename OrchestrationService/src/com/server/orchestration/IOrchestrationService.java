7982980e25ffb86ba1ac08782c10c3af57d91cd9
Yusuf Akdas <yusufakdas@gtu.edu.tr>
2018-12-25 21:09:34 +0300

    Duzenlemeler yapildi.

--- OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
+++ OrchestrationService/src/com/server/orchestration/IOrchestrationService.java
@@ -2,8 +2,9 @@ package com.server.orchestration;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
-import com.db.biztalk.*;
+import com.biztalk.dao.*;
 
 import javax.xml.bind.annotation.XmlElement;
 import java.util.List;
@@ -16,27 +17,12 @@ public interface IOrchestrationService {
      * @param value Object that contains orchestration information.
      * @param jobs List that contains jobs of orchestration.
      * @param rules List that contains rules of orchestration.
-     * @return If added true, otherwise false.
+     * @return Message.
      */
     @WebMethod(action = "add_orchestration", operationName = "addOrchestration")
-    boolean addOrchestration(@WebParam(name = "orchVal")  @XmlElement(required = true) Orchestration value,
-                             @WebParam(name = "jobList")  @XmlElement(required = true) List<Job> jobs,
-                             @WebParam(name = "ruleList") @XmlElement(required = true) List<Rule> rules);
-
-    /**
-     * Add given job to database.
-     * @param value Job to be added to database.
-     * @return If added true, otherwise false.
-     */
-    @WebMethod(action = "add_job", operationName = "addJob")
-    boolean addJob(@WebParam(name = "job") @XmlElement(required = true) Job value);
-
-    /**
-     * Add given rule to database.
-     * @param value Rule to be added to database.
-     * @return If added true, otherwise false.
-     */
-    @WebMethod(action = "add_rule", operationName = "addRule")
-    boolean addRule(@WebParam(name = "rule") @XmlElement(required = true) Rule value);
+    @WebResult(name = "message")
+    String addOrchestration(@WebParam(name = "orchestration") @XmlElement(required = true) Orchestration value,
+                            @WebParam(name = "jobList")       @XmlElement(required = true) List<Job> jobs,
+                            @WebParam(name = "ruleList")      @XmlElement(required = true) List<Rule> rules);
 
 }
--- OrchestrationService/src/com/server/orchestration/OrchestrationService.java
+++ OrchestrationService/src/com/server/orchestration/OrchestrationService.java
@@ -1,6 +1,6 @@
 package com.server.orchestration;
 
-import com.db.biztalk.*;
+import com.biztalk.dao.*;
 
 import javax.jws.WebService;
 import java.text.ParseException;
@@ -13,74 +13,94 @@ import java.util.List;
             serviceName = "OrchestrationService")
 public class OrchestrationService implements IOrchestrationService {
 
+    /**
+     * For accessing Job table
+     */
+    private JobDAO jobDAO = new JobDAO();
+
+    /**
+     * For accessing Rule table
+     */
+    private RuleDAO ruleDAO = new RuleDAO();
+
+    /**
+     * For accessing Orchestration table
+     */
+    private OrchestrationDAO orchestrationDAO = new OrchestrationDAO();
+
+    /**
+     * Introduce an orchestration.
+     * @param value Object that contains orchestration information.
+     * @param jobs List that contains jobs of orchestration.
+     * @param rules List that contains rules of orchestration.
+     * @return Message.
+     */
     @Override
-    public boolean addOrchestration(Orchestration value, List<Job> jobs, List<Rule> rules) {
+    public String addOrchestration(Orchestration value, List<Job> jobs, List<Rule> rules) {
         List<Integer> JobIdList = new ArrayList<>();
-    	List<Integer> RuleIdList = new ArrayList<>();
+        List<Integer> RuleIdList = new ArrayList<>();
 
     	//End nodes
-    	JobIdList.add(0);
-    	RuleIdList.add(0);
+        JobIdList.add(0);
+        RuleIdList.add(0);
     	
     	//Saving jobs to the database
     	// and adding their generated id's from db to JobIdList.
-    	for(Job temp : jobs)
-    		JobIdList.add(addJobSub(temp));
+        for(Job temp : jobs)
+            JobIdList.add(addJob(temp));
 
     	//Saving rules to db.
     	// creating edges between rules declared from gui.
     	// adding Rule id's to the RuleIdList.
-    	for(Rule temp : rules)
-    	{
-    		temp.setYesEdge(JobIdList.get(temp.getYesEdge()));
-    		temp.setNoEdge(JobIdList.get(temp.getNoEdge()));
-    		RuleIdList.add(this.addRuleSub(temp));
-    	}
+        for(Rule temp : rules)
+        {
+            temp.setYesEdge(JobIdList.get(temp.getYesEdge()));
+            temp.setNoEdge(JobIdList.get(temp.getNoEdge()));
+            RuleIdList.add(this.addRule(temp));
+        }
     	
     	//Updating all jobs with their associated rules with db ids.
-    	for(int search = 1; search < JobIdList.size(); ++search)
+        for(int search = 1; search < JobIdList.size(); ++search)
     	{
-    		JobDAO accessingDb = new JobDAO();
-    		Job workon;
+    	    Job workOn;
 			try {
-				workon = accessingDb.getJob(search);
+                workOn = jobDAO.getJob(search);
 			} catch (Exception e1) {
-				System.err.println("Error to access given id");
-				return false;
+                System.err.println("Error to access given id: " + search);
+                return String.format("*** Error to access given id: %d***", search);
 			}
-    		try {
-				accessingDb.UpdateJob(search, "RuleId", RuleIdList.get(workon.getRuleId() - 1));
-			} catch (Exception e) {
-				System.err.println("Error to access given id");
-				return false;
+
+            try {
+                jobDAO.UpdateJob(search, "RULE_ID", RuleIdList.get(workOn.getRuleId() - 1));
+            } catch (Exception e) {
+                System.err.println("Error to access given id: " + RuleIdList.get(workOn.getRuleId() - 1));
+                return String.format("*** Error to access given id: %d***", RuleIdList.get(workOn.getRuleId() - 1));
 			}
     	}
 
+    	// Set the start job's id.
+    	value.setStartJobID(JobIdList.get(1));
+
     	// Adding orchestration value to db.
-    	try {
-            OrchestrationDAO orchestrationDAO = new OrchestrationDAO();
+        try {
             orchestrationDAO.insertOrchestration(value);
         } catch (Exception e) {
     	    System.err.println("Orchestration could not be introduced: " + e);
-            return false;
+            return "*** Orchestration could not be introduced. ***";
     	}
-
-        return true;
+        return "Orchestration has been introduced successfully!";
     }
 
-    @Override
-    public boolean addJob(Job value){
-        return addJobSub(value) > 0;
-    }
-
-    @Override
-    public boolean addRule(Rule value){
-        return addRuleSub(value) > 0;
-    }
-
+    /**
+     * Add given job to database.
+     * @param value Job to be added to database.
+     * @return If added, returns job id which is got from db, otherwise -1 to indicate an error.
+     */
     // Bu method eklenen job ın id sini döndürmeli ancak dbden öyle bi bilgi alamıyorum.
-    private int addJobSub(Job value){
-        try{
+    private int addJob(Job value){
+        int dbJobId;
+
+        try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
             String date = dateFormat.format(new Date());
             value.setInsertDateTime(date);
@@ -92,37 +112,36 @@ public class OrchestrationService implements IOrchestrationService {
 
             //RuleID GUI den doğru geldiğini düşünüyorum. Aksi takdirde db tarafından üretilen RuleID yi bi şekilde alabilmeliyim.
             //value.setRuleId(0);
-        }
-        catch(ParseException e){
+        } catch(ParseException e){
             System.out.println("Date Parse error" + e);
 
             return -1;
         }
 
-        JobDAO jobDB = new JobDAO();
         try {
-            jobDB.insertJob(value);
-        }
-        catch(Exception e){
+            dbJobId = jobDAO.insertJob(value);
+        } catch(Exception e){
             System.out.println("Job Db insert error: " + e);
-
             return -1;
         }
-
-        return 1;
+        return dbJobId;
     }
 
-    private int addRuleSub(Rule value){
-        RuleDAO ruleDB = new RuleDAO();
+    /**
+     * Add given rule to database.
+     * @param value Rule to be added to database.
+     * @return If added, returns rule id which is got from db, otherwise -1 to indicate an error.
+     */
+    private int addRule(Rule value){
+        int dbRuleId;
+
         try {
-            ruleDB.insertRule(value);
-        }
-        catch(Exception e){
+            dbRuleId = ruleDAO.insertRule(value);
+        } catch(Exception e){
             System.out.println("Rule Db insert error: " + e);
             return -1;
         }
-
-        return 1;
+        return dbRuleId;
     }
 
 }
